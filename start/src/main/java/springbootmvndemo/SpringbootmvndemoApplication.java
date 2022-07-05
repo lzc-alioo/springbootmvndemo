6716ae7f19305be2ca23ca3959d186c47b7ff52a
悟心 <zhichong.lzc@alibaba-inc.com>
2022-07-05 23:16:56 +0800

    init

---
+++ start/src/main/java/springbootmvndemo/SpringbootmvndemoApplication.java
@@ -0,0 +1,13 @@
+package springbootmvndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmvndemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootmvndemoApplication.class, args);
+    }
+
+}
